6184a9b963bd4835bc1b34424db02b8a7c70faef
sergeynn <sergey_noskov@yahoo.com>
2020-08-18 07:32:30 +0500

    HW1

---
+++ src/main/java/ru/javawebinar/topjava/repository/ImplUserMeal.java
@@ -0,0 +1,27 @@
+package ru.javawebinar.topjava.repository;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.Collection;
+
+public class ImplUserMeal implements UserMealRepository{
+    @Override
+    public Meal save(Meal meal) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public Meal getId(int id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Meal> getAll() {
+        return null;
+    }
+}
